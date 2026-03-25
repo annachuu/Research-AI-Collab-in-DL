6ba0172749a3e22381b133eade186e92bb5c1722
annachuu <annachu211@gmail.com>
2026-03-25 12:56:50 -0600

    temp credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect("mongodb+srv://annachu:annaichu@sddlcluster.yblin3v.mongodb.net/serendipitiesDB?retryWrites=true&w=majority&")
+        const conn = await mongoose.connect("mongodb+srv://username:password@sddlcluster.yblin3v.mongodb.net/serendipitiesDB?retryWrites=true&w=majority&")
 
         console.log(`MongoDB is connected: ${conn.connection.host}`.cyan.underline);
     }catch (error) {
